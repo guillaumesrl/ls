1d2f21cb83a7a2a7c9dd1868670b7f08f27a5593
Guillaume Sorel <guillaumesrl@gmail.com>
2019-05-18 23:44:18 +0200

    launchschool basics

---
+++ 1_bases_square_float.rb
@@ -0,0 +1,6 @@
+a = 3
+
+3.times { |x| a = 4 
+	b = 2 }
+puts a 
+puts b
---
+++ 2_variables_name.rb
@@ -0,0 +1,6 @@
+puts "Enter your name please"
+first_name = gets.chomp
+puts "enter your last name please"
+last_name = gets.chomp
+
+puts first_name + " " + last_name
